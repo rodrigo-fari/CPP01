bdf87ac8a7bfefab38a4fbbc919dc9a6baee7c9c
rodrigo-fari <rodrigofal44@gmail.com>
2025-06-24 18:11:52 +0100

    Created Zombie class

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,24 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 17:47:04 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/06/24 17:47:05 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/06/24 18:11:14 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie {
+	private:
+		std::string name;
+
+	public:
+		Zombie( std::string name );
+		~Zombie();
+		void	announce( void );
+};
+
+#endif
